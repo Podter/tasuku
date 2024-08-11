9ff7c10430605f123fccc5645809527b692bfc1b
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-12 02:08:08 +0700

    Update session-store.ts

--- src/lib/session-store.ts
+++ src/lib/session-store.ts
@@ -1,6 +1,6 @@
 import * as SecureStore from "expo-secure-store";
 
-const key = "token";
+const key = "session-id";
 export const getToken = () => SecureStore.getItem(key);
 export const deleteToken = () => SecureStore.deleteItemAsync(key);
 export const setToken = (v: string) => SecureStore.setItem(key, v);
