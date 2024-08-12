4d851562303661442b19a734e62c55efa4105e2e
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-13 01:09:11 +0700

    Update schema.ts

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,11 +1,14 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgTable, primaryKey, text, timestamp } from "drizzle-orm/pg-core";
 
+// User
 export const User = pgTable("user", {
   id: text("id").primaryKey(),
   username: text("username").notNull().unique(),
   password: text("password").notNull(),
 });
 
+// Session
 export const Session = pgTable("session", {
   id: text("id").primaryKey(),
   userId: text("user_id")
@@ -16,3 +19,51 @@ export const Session = pgTable("session", {
     mode: "date",
   }).notNull(),
 });
+
+// List
+export const List = pgTable("list", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  userId: text("user_id").references(() => User.id),
+});
+
+export const ListRelations = relations(List, ({ many }) => ({
+  ListToTask: many(ListToTask),
+}));
+
+// Task
+export const Task = pgTable("task", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+});
+
+export const TaskRelations = relations(Task, ({ many }) => ({
+  ListToTask: many(ListToTask),
+}));
+
+// ListToTask
+export const ListToTask = pgTable(
+  "list_to_task",
+  {
+    listId: text("list_id")
+      .notNull()
+      .references(() => List.id),
+    taskId: text("task_id")
+      .notNull()
+      .references(() => Task.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.listId, t.taskId] }),
+  }),
+);
+
+export const ListToTaskRelations = relations(ListToTask, ({ one }) => ({
+  List: one(List, {
+    fields: [ListToTask.listId],
+    references: [List.id],
+  }),
+  Task: one(Task, {
+    fields: [ListToTask.taskId],
+    references: [Task.id],
+  }),
+}));
