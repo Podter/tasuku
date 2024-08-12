ede9f7749e76e04b5852fd11aadb5bc9258fd3f4
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-13 02:49:20 +0700

    Update index.ts

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,5 +3,20 @@ import postgres from "postgres";
 
 import * as schema from "./schema";
 
-const queryClient = postgres(process.env.POSTGRES_URL!);
-export const db = drizzle(queryClient, { schema });
+function singleton<T>(name: string, value: () => T): T {
+  const globalAny: any = global;
+  globalAny.__singletons = globalAny.__singletons || {};
+
+  if (!globalAny.__singletons[name]) {
+    globalAny.__singletons[name] = value();
+  }
+
+  return globalAny.__singletons[name];
+}
+
+function createDrizzle() {
+  const queryClient = postgres(process.env.POSTGRES_URL!);
+  return drizzle(queryClient, { schema });
+}
+
+export const db = singleton("db", createDrizzle);
