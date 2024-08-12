e2142d4be7230cc0a01d40b98954542d1561c6a8
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-12 13:14:35 +0700

    Update session-store.ts

--- src/lib/session-store.ts
+++ src/lib/session-store.ts
@@ -1,6 +1,25 @@
+import { Platform } from "react-native";
 import * as SecureStore from "expo-secure-store";
 
-const key = "session-id";
-export const getToken = () => SecureStore.getItem(key);
-export const deleteToken = () => SecureStore.deleteItemAsync(key);
-export const setToken = (v: string) => SecureStore.setItem(key, v);
+const KEY = "session-id";
+
+export function getToken() {
+  if (Platform.OS === "web") {
+    return localStorage.getItem(KEY);
+  }
+  return SecureStore.getItem(KEY);
+}
+
+export async function deleteToken() {
+  if (Platform.OS === "web") {
+    return localStorage.removeItem(KEY);
+  }
+  await SecureStore.deleteItemAsync(KEY);
+}
+
+export async function setToken(value: string) {
+  if (Platform.OS === "web") {
+    return localStorage.setItem(KEY, value);
+  }
+  await SecureStore.setItemAsync(KEY, value);
+}
