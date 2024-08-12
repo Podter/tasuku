aa1cacfbfe9e4caa322aef6c77507c5825ddf8ca
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-12 17:16:38 +0700

    Create use-keyboard.ts

---
+++ src/hooks/use-keyboard.ts
@@ -0,0 +1,51 @@
+import type { KeyboardEvent } from "react-native";
+import { useEffect, useState } from "react";
+import { Keyboard } from "react-native";
+
+const EVENT_TYPE = {
+  didShow: { show: "keyboardDidShow", hide: "keyboardDidHide" },
+  willShow: { show: "keyboardWillShow", hide: "keyboardWillHide" },
+} as const;
+
+export function useKeyboard(
+  { eventType = "didShow" }: { eventType?: keyof typeof EVENT_TYPE } = {
+    eventType: "didShow",
+  },
+) {
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
+  useEffect(() => {
+    const showListener = Keyboard.addListener(
+      EVENT_TYPE[eventType].show,
+      (e: KeyboardEvent) => {
+        setKeyboardVisible(true);
+        setKeyboardHeight(e.endCoordinates.height);
+      },
+    );
+    const hideListener = Keyboard.addListener(
+      EVENT_TYPE[eventType].hide,
+      () => {
+        setKeyboardVisible(false);
+        setKeyboardHeight(0);
+      },
+    );
+
+    return () => {
+      showListener.remove();
+      hideListener.remove();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function dismissKeyboard() {
+    Keyboard.dismiss();
+    setKeyboardVisible(false);
+  }
+
+  return {
+    isKeyboardVisible,
+    keyboardHeight,
+    dismissKeyboard,
+  };
+}
