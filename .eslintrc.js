1450d649f0912d6fcb9a5b528813fedae22193bc
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-12-01 23:10:42 +0700

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,25 @@
 module.exports = {
   extends: ["expo", "prettier"],
   plugins: ["prettier", "eslint-plugin-react-compiler"],
-  ignorePatterns: ["/dist/*"],
+  ignorePatterns: [
+    "/dist/*",
+    "/.expo/*",
+    "/.tamagui/*",
+    "node_modules",
+    ".eslintrc.js",
+    "babel.config.js",
+  ],
   rules: {
     "prettier/prettier": "warn",
     "react-compiler/react-compiler": "warn",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      { prefer: "type-imports", fixStyle: "separate-type-imports" },
+    ],
+    "import/consistent-type-specifier-style": ["warn", "prefer-top-level"],
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
   },
 };
