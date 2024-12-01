83baa7648cb6dc0efe491fb3aa6a225dc3d5fab4
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-12-01 22:30:24 +0700

    added tamagui

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,59 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { StatusBar, useColorScheme } from "react-native";
+import { TamaguiProvider, useTheme } from "tamagui";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import { tamaguiConfig } from "../lib/tamagui";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const colorScheme = useColorScheme();
+
+  const [fontLoaded, fontError] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  useEffect(() => {
+    if (fontLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded, fontError]);
+
+  if (!fontLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <StatusBar
+          barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+        />
+        <App />
+      </ThemeProvider>
+    </TamaguiProvider>
+  );
+}
+
+function App() {
+  const theme = useTheme();
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: theme.background.val,
+        },
+      }}
+    />
+  );
 }
