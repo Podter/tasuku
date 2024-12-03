0c9f4f23009eaceaaca335394f259f7b1660e35f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-12-03 19:40:53 +0700

    Update todo-item.tsx

--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -1,29 +1,62 @@
-import { useState } from "react";
 import { Check as CheckIcon } from "@tamagui/lucide-icons";
-import { Checkbox, ListItem } from "tamagui";
+import { Checkbox, ListItem, Spinner, Square } from "tamagui";
+
+import { api } from "~/lib/api";
 
 interface TodoItemProps {
   id: string;
 }
 
 export default function TodoItem({ id }: TodoItemProps) {
-  const [done, setDone] = useState(false);
+  const utils = api.useUtils();
+  const { data, isLoading } = api.task.getOne.useQuery({ id });
+
+  const { mutate } = api.task.update.useMutation({
+    onMutate: async (newData) => {
+      await utils.task.getOne.cancel({ id });
+      const previousData = utils.task.getOne.getData({ id });
+      if (previousData) {
+        utils.task.getOne.setData({ id }, { ...previousData, ...newData });
+        return { previousData };
+      }
+    },
+    onError: (_err, _newData, context) => {
+      if (context?.previousData) {
+        utils.task.getOne.setData({ id }, context.previousData);
+      }
+    },
+    onSettled: () => {
+      utils.task.getOne.invalidate({ id });
+    },
+  });
+
+  if (!data || isLoading) {
+    return (
+      <ListItem
+        icon={({ color }) => <Spinner color={color} />}
+        title={<Square h={17} w="$15" bg="$borderColor" borderRadius="$2" />}
+      />
+    );
+  }
 
   return (
     <ListItem
       hoverTheme
       pressTheme
-      onPress={() => setDone((v) => !v)}
+      onPress={() => mutate({ id, done: !data.done })}
       icon={() => (
-        <Checkbox checked={done} onPress={() => setDone((v) => !v)}>
+        <Checkbox
+          checked={data.done}
+          onPress={() => mutate({ id, done: !data.done })}
+        >
           <Checkbox.Indicator>
             <CheckIcon />
           </Checkbox.Indicator>
         </Checkbox>
       )}
       title={
-        <ListItem.Text textDecorationLine={done ? "line-through" : "none"}>
-          Do laundry
+        <ListItem.Text textDecorationLine={data.done ? "line-through" : "none"}>
+          {data.title}
         </ListItem.Text>
       }
     />
