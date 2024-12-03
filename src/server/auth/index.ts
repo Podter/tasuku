3637e875db33e958bb8cac1c44522d3eaef41001
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-12-03 23:19:13 +0700

    Update index.ts

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -2,7 +2,6 @@ import { expo } from "@better-auth/expo";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { anonymous } from "better-auth/plugins";
-import { eq } from "drizzle-orm";
 
 import { db } from "../db";
 import * as schema from "../db/schema";
