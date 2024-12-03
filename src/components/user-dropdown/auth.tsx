037aa2e54b1494dd4343367b33929180fc624e75
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-12-03 23:23:49 +0700

    Update auth.tsx

--- src/components/user-dropdown/auth.tsx
+++ src/components/user-dropdown/auth.tsx
@@ -1,15 +1,42 @@
 import { Link } from "expo-router";
-import { LogIn as LogInIcon } from "@tamagui/lucide-icons";
+import {
+  LogIn as LogInIcon,
+  LogOut as LogOutIcon,
+} from "@tamagui/lucide-icons";
 import { ListItem, YGroup } from "tamagui";
 
 import type { WithCloseDropdown } from "./user-dropdown";
+import { authClient } from "~/lib/auth-client";
 
 export default function Auth({ closeDropdown }: WithCloseDropdown) {
+  const { data, isPending } = authClient.useSession();
+
+  if (isPending) {
+    return null;
+  }
+
+  if (!data || data.user.isAnonymous) {
+    return (
+      <YGroup.Item>
+        <Link href="/auth" asChild onPress={() => closeDropdown()}>
+          <ListItem hoverTheme pressTheme icon={LogInIcon} title="Sign in" />
+        </Link>
+      </YGroup.Item>
+    );
+  }
+
   return (
     <YGroup.Item>
-      <Link href="/auth" asChild onPress={() => closeDropdown()}>
-        <ListItem hoverTheme pressTheme icon={LogInIcon} title="Sign in" />
-      </Link>
+      <ListItem
+        onPress={() => {
+          authClient.signOut();
+          closeDropdown();
+        }}
+        hoverTheme
+        pressTheme
+        icon={LogOutIcon}
+        title="Sign out"
+      />
     </YGroup.Item>
   );
 }
